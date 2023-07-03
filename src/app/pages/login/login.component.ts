692b159e9b90b87a347bf6175e9251eb9d97fa31
Nayanne Batista <nayannelopes.bd@gmail.com>
2023-07-03 15:52:21 -0300

    Aula 1 - terceiro vídeo - Validando o login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AutenticacaoService } from 'src/app/core/services/autenticacao.service';
 
@@ -20,23 +20,25 @@ export class LoginComponent implements OnInit{
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
-      email: [null],
-      senha: [null]
+      email: [null, [Validators.required, Validators.email]],
+      senha: [null, Validators.required]
     })
   }
 
   login() {
-    const email = this.loginForm.value.email;
-    const senha = this.loginForm.value.senha;
-    this.authService.autenticar(email, senha).subscribe({
-      next: (value) => {
-        console.log('Autenticado com sucesso', value)
-        this.router.navigateByUrl('/')
-        this.loginForm.reset();
-      },
-      error: (err) => {
-        console.log('Problema na autenticação', err)
-      },
-    })
+    if(this.loginForm.valid) {
+      const email = this.loginForm.value.email;
+      const senha = this.loginForm.value.senha;
+      this.authService.autenticar(email, senha).subscribe({
+        next: (value) => {
+          console.log('Autenticado com sucesso', value)
+          this.router.navigateByUrl('/')
+          this.loginForm.reset();
+        },
+        error: (err) => {
+          console.log('Problema na autenticação', err)
+        },
+      })
+    }
   }
 }
